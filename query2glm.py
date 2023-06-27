8368b3a4757eeb6add3864578d3ff02897459fad
XuekaiChen <50224248+XuekaiChen@users.noreply.github.com>
2023-06-27 15:29:28 +0800

    回答中加入参考依据

--- query2glm.py
+++ query2glm.py
@@ -6,6 +6,7 @@ import requests
 import json
 import jieba.analyse
 import re
+import pickle
 import os
 import faiss  # 注意，这个不能在引用BERT之前引入
 
@@ -16,8 +17,11 @@ class ChatGLM:
         self.chatglm6b_url = chat_url
         self.encode_model = encode_model
 
-    def post_to_6b(self, article, question, history):
-        history_prompt = ""
+    def post_to_6b(self, article, page, question, history):
+        if page:
+            article = f'依据来自文本第{page}页段落：{article}'
+
+        history_prompt = ""  # TODO history prompt是否还需要
         history = history[-5:]  # 限制近5轮对话
         for i in range(len(history)):
             history_prompt = history_prompt + "Q：" + history[i][0] + "\n"
@@ -28,7 +32,7 @@ class ChatGLM:
         A：知道了，我会严格按照您所提供的知识回答问题。\
         {article}
         {history_prompt}
-        Q：{question}，回答之前让我们先想一想你的答案是否有依据。\
+        Q：{question}，回答的最后给出参考依据。\
         A：
         """
 
@@ -40,60 +44,29 @@ class ChatGLM:
             return "ChatGLM访问失败，无法提供答案。"
         result = json.loads(res.text)
         print(result)
-        # 根据关键词判断
-        # result["response"] = self.result_clean(article + history_prompt, result["response"])
-        # TODO 处理返回结果
         return result["response"]
-
-    def result_clean(self, article, result):
-        # TODO 这个在干啥？可能不是很必要
-        # 语句分拆
-        result_sen = re.split(r"([。?？!！])", result)
-        result_sen.append("")
-        result_sen = ["".join(i) for i in zip(result_sen[0::2], result_sen[1::2])]
-
-        # 逐句检验
-        clean_sen = []
-        for sen in result_sen:
-            # 过短的句子不检验
-            if len(sen) < 10:
-                clean_sen.append(sen)
-                continue
-            # 通过模型创造的关键词占比验证
-            sen_kw = jieba.analyse.extract_tags(sen,
-                                                topK=max(int(0.05 * len(sen)), 3),
-                                                withWeight=False)
-
-            sen_kw = list(set(sen_kw))
-            create_num = 0
-            for kw in sen_kw:
-                if kw not in article:
-                    create_num += 1
-            if create_num > 0.5 * len(sen_kw):
-                print(create_num, len(sen_kw), sen_kw, sen)
-                continue
-            else:
-                clean_sen.append(sen)
-
-        # 生成结果
-        if len(clean_sen) < 0.3 * len(result_sen):
-            clean_res = "您问的问题在所给文本中没有答案，以下是我可以提供的答案：\n" + article
-        else:
-            clean_res = "".join(clean_sen)
-
-        return clean_res
 
     async def generate(self, faq_id: str, query: str, history):
         # 找最相似的上下文article
         index_db = faiss.read_index(os.path.join(self.faiss_path, faq_id, "index_db.index"))
         with open(os.path.join(self.faiss_path, faq_id, "context_pair.json"), 'r') as jsonfile:
             qa_pair_list = json.load(jsonfile)
+        with open(os.path.join(self.faiss_path, faq_id, "paragraph_pages.data"), "rb") as f:
+            para_pages = pickle.load(f)
+
         query_embedding = self.encode_model.encode([query])
-        _, top_id = index_db.search(query_embedding, 2)  # TODO 加上confidence判断，剔除无关的上下文
+        # TODO 添加跨范围回答功能，并标注依据（建立索引时加入页码）
+        #  方法：查找top3相关上下文，用confidence剔除无关的上下文
+        _, top_id = index_db.search(query_embedding, 2)  # top_id:(1, top_k)  
         most_sim_id = int(top_id[0][0])
         context = qa_pair_list[most_sim_id][1]
 
+        # 加入参考页码
+        page_ref = None
+        if para_pages:
+            page_ref = para_pages[most_sim_id]
+
         # post_to_6b
-        answer = self.post_to_6b(context, query, history)
+        answer = self.post_to_6b(context, page_ref, query, history)
 
         return answer
